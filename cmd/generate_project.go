0fc5fcf13b0b0a1ef23d46f4d931781e200c160d
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2024-07-30 15:23:37 +0545

    feat: generate infrastructure as well when generating project

--- cmd/generate_project.go
+++ cmd/generate_project.go
@@ -21,6 +21,7 @@ var projectFlags = []utils.FlagItem{
 	{Name: "name", Short: "n", Desc: "project name"},
 	{Name: "desc", Short: "e", Desc: "project description"},
 	{Name: "pkg", Short: "p", Desc: "package name"},
+	{Name: "infratype", Short: "i", Desc: "infastructure types (eg; aws, firebase, gcp, gmail, opensearch)", Def: []string{}},
 }
 
 func init() {
@@ -48,6 +49,23 @@ func createProject(cmd *cobra.Command, args []string) {
 	generator := gen.NewProjectGenerator(input, templates.FS)
 	if err := generator.Generate(); err != nil {
 		logger.Fatal("project generation failed.", "err", err)
+	}
+
+  // generate infrastructure after project has been generated 
+	infraInput := pkg.GetConfig[models.Infrastructure]()
+	infraInput.ProjectModuleName = input.ProjectModuleName
+	infraInput.Directory = input.Directory
+
+	logger.Infof("infra input: %v", infraInput)
+
+	// validate input data
+	if err := infraInput.Validate(); err != nil {
+		logger.Fatal("validation for input failed.", "err", err)
+	}
+
+	infraGenerator := gen.NewInfraGenerator(infraInput, templates.FS)
+	if err := infraGenerator.Generate(); err != nil {
+		logger.Fatal("infrastructure generation failed.", "err", err)
 	}
 
 }
